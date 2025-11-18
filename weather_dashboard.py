25b0501183aad0b7d7dfa140680db0854ed5e285
TWFish <157035974+TWFish@users.noreply.github.com>
2025-11-18 18:04:15 +0800

    Update weather_dashboard.py

--- weather_dashboard.py
+++ weather_dashboard.py
@@ -7,7 +7,7 @@ LOCATION = st.selectbox("選擇城市", ["Taipei","Taichung","Kaohsiung"])
 
 ulr = 'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-EE0FA5AF-16E7-4E77-80A0-97172349E1A5&locationName=%E8%87%BA%E5%8C%97%E5%B8%82'
 
-data = requests.get(ulr).json()
+data = requests.get(ulr,verify=False).json()
 location = data['records']['location'][0]
 st.subheader(f"{location['locationName']}36小時預報")
 
