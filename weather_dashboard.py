386ddba90f75c6204b770c15b2853ad1d4ab7fe4
TWFish <157035974+TWFish@users.noreply.github.com>
2025-12-29 12:18:03 +0800

    Update weather_dashboard.py

--- weather_dashboard.py
+++ weather_dashboard.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.title("台灣氣象資料 Dashboard")
 LOCATION = st.selectbox("選擇城市", ["Taipei","Taichung","Kaohsiung"])
 
-ulr = 'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-EE0FA5AF-16E7-4E77-80A0-97172349E1A5&locationName=%E8%87%BA%E5%8C%97%E5%B8%82'
+ulr = 'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-EE0FA5AF-16E7-4E77-80A0-97172349E1A5'
 
 data = requests.get(ulr,verify=False).json()
 location = data['records']['location'][0]
