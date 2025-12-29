61fddadefb14b6b8b87e74ba11d316471537bf73
TWFish <157035974+TWFish@users.noreply.github.com>
2025-12-29 12:33:12 +0800

    Update weather_dashboard.py

--- weather_dashboard.py
+++ weather_dashboard.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 
 st.title("台灣氣象資料 Dashboard")
-LOCATION = st.selectbox("選擇城市", ["Taipei","Taichung","Kaohsiung"])
+LOCATION = st.selectbox("選擇城市", ["臺北","臺中","高雄"])
 
 ulr = 'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-EE0FA5AF-16E7-4E77-80A0-97172349E1A5'
 
